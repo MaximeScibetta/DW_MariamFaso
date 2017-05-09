dfb496597708e8c0df2c4255780250af61c3c905
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-05-09 12:34:31 +0200

    Horitonal rythm on HomePage and strart ProjectPage

--- src/js/statistics.js
+++ src/js/statistics.js
@@ -4,32 +4,30 @@ var data1 = {
         "Outils manquant",
         "Outils donnés"
     ],
-    datasets: [
-        {
-            data: [350, 100],
-            backgroundColor: [
-                "#f0776c",
-                "#ffd071",
-               // "#3498db",
-                //"#1abc9c"
-            ],
-            hoverBackgroundColor: [
-                "#ff685b",
-                "#FFCE56",
-               // "#36A2EB"
-            ]
-        }]
+    datasets: [{
+        data: [350, 100],
+        backgroundColor: [
+            "#f0776c",
+            "#ffd071",
+            // "#3498db",
+            //"#1abc9c"
+        ],
+        hoverBackgroundColor: [
+            "#ff685b",
+            "#FFCE56",
+            // "#36A2EB"
+        ]
+    }]
 };
 
 var ctx1 = document.getElementById("asblStats").getContext("2d");
 //ctx1.canvas.width = 400;
 //ctx1.canvas.height = 400;
-var myDoughnut = new Chart(ctx1,{
+var myDoughnut = new Chart(ctx1, {
     type: 'doughnut',
     data: data1,
-    options: {
-        }
-    });
+    options: {}
+});
 /************************2e SECTION ********************* */
 /************************2e SECTION ********************* */
 /************************2e SECTION ********************* */
@@ -39,35 +37,33 @@ var data2 = {
         "Blue",
         "Yellow"
     ],
-    datasets: [
-        {
-            data: [300, 50, 200],
-            backgroundColor: [
-                "#f0776c",
-                "#ffd071",
-                "#3498db",
-                "#1abc9c"
-            ],
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#FFCE56",
-                "#36A2EB"
-            ],
-            labels: {
-                legend: "test"
-            }
-        }]
+    datasets: [{
+        data: [300, 50, 200],
+        backgroundColor: [
+            "#f0776c",
+            "#ffd071",
+            "#3498db",
+            "#1abc9c"
+        ],
+        hoverBackgroundColor: [
+            "#FF6384",
+            "#FFCE56",
+            "#36A2EB"
+        ],
+        labels: {
+            legend: "test"
+        }
+    }]
 };
 var ctx2 = document.getElementById("asblStat1").getContext("2d");
-ctx2.canvas.width = 400;
-ctx2.canvas.height = 400;
-var myDoughnut = new Chart(ctx2,{
+ctx2.canvas.width = 250;
+ctx2.canvas.height = 250;
+var myDoughnut = new Chart(ctx2, {
     type: 'doughnut',
     data: data2,
-    options: {
-        }
-    });
-    
+    options: {}
+});
+
 /***********************2e section 2eme stat************************* */
 var data3 = {
     labels: [
@@ -75,35 +71,33 @@ var data3 = {
         "Blue",
         "Yellow"
     ],
-    datasets: [
-        {
-            data: [300, 50, 200],
-            backgroundColor: [
-                "#f0776c",
-                "#ffd071",
-                "#3498db",
-                "#1abc9c"
-            ],
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#FFCE56",
-                "#36A2EB"
-            ],
-            labels: {
-                legend: "test"
-            }
-        }]
+    datasets: [{
+        data: [300, 50, 200],
+        backgroundColor: [
+            "#f0776c",
+            "#ffd071",
+            "#3498db",
+            "#1abc9c"
+        ],
+        hoverBackgroundColor: [
+            "#FF6384",
+            "#FFCE56",
+            "#36A2EB"
+        ],
+        labels: {
+            legend: "test"
+        }
+    }]
 };
 var ctx3 = document.getElementById("asblStat2").getContext("2d");
 ctx3.canvas.width = 400;
 ctx3.canvas.height = 400;
-var myDoughnut = new Chart(ctx3,{
+var myDoughnut = new Chart(ctx3, {
     type: 'doughnut',
     data: data3,
-    options: {
-        }
-    });
-    
+    options: {}
+});
+
 /**********************3e stats********************* */
 var data4 = {
     labels: [
@@ -111,31 +105,29 @@ var data4 = {
         "Blue",
         "Yellow"
     ],
-    datasets: [
-        {
-            data: [300, 50, 200],
-            backgroundColor: [
-                "#f0776c",
-                "#ffd071",
-                "#3498db",
-                "#1abc9c"
-            ],
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#FFCE56",
-                "#36A2EB"
-            ],
-            labels: {
-                legend: "test"
-            }
-        }]
+    datasets: [{
+        data: [300, 50, 200],
+        backgroundColor: [
+            "#f0776c",
+            "#ffd071",
+            "#3498db",
+            "#1abc9c"
+        ],
+        hoverBackgroundColor: [
+            "#FF6384",
+            "#FFCE56",
+            "#36A2EB"
+        ],
+        labels: {
+            legend: "test"
+        }
+    }]
 };
 var ctx4 = document.getElementById("asblStat3").getContext("2d");
 ctx4.canvas.width = 400;
 ctx4.canvas.height = 400;
-var myDoughnut = new Chart(ctx4,{
+var myDoughnut = new Chart(ctx4, {
     type: 'doughnut',
     data: data4,
-    options: {
-        }
-    });
+    options: {}
+});
